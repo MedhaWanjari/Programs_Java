62785e8dee30820a3aa95ff774dbf046b40eb1f7
Medha Wanjari <medhawanjari2013@gmail.com>
2025-06-15 18:31:43 +0530

    15-06-2025

---
+++ src/ex_24_Exceptions/Lab208_Bad.java
@@ -0,0 +1,20 @@
+package ex_24_Exceptions;
+
+public class Lab208_Bad {
+    public static void main(String[] args) {
+        int c = 0;
+        int b = 0;
+        try {
+            b = 10/c;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(b);
+    }
+}
