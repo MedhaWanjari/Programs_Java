e72dc4f069d13dd0c0abac7d2a85e6f1e9a5ad9e
Medha Wanjari <medhawanjari2013@gmail.com>
2025-06-15 19:51:15 +0530

    15-06-2025 part 2

---
+++ src/ex_24_Exceptions/Lab216.java
@@ -0,0 +1,16 @@
+package ex_24_Exceptions;
+
+public class Lab216 {
+    public static void main(String[] args) {
+        try {
+            String ip = args[0]; // java.lang.ArrayIndexOutOfBoundsException
+            int a = Integer.parseInt(ip); // NumberFormatException
+            int b = 100 / a; // ArithmeticException
+            System.out.println(b);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
